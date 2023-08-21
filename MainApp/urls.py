fa476e937259518eb4890b7d38e74bc6f777a4aa
Ormizz <urielsoro@gmail.com>
2023-08-22 00:27:29 +0200

    Katchi

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('Login', views.connexion,name='login'),
     path('SignUp', views.SignUp ,name='SignUp'),
     path('Logout', views.deconnexion,name='logout'),
+    path('profileU', views.profileU,name='profileU'),
+    path('profileU/editUser', views.editUser, name='edit-User'),
     path('image', views.enregistrer_photo,name='photo_test'),
     path('afficher_photos', views.afficher_photos,name='afficher_photos'),
 ]
